104f64077cc19b62763c14fdf093263d7045cd63
Reza Amuzadeh <amuzadeh@vasl.ir>
2021-12-04 18:20:31 +0330

    added undefined view type

--- ChatKitV4Core/src/main/java/ir/vasl/chatkitv4core/model/chatkitv4enums/MessageContentType.kt
+++ ChatKitV4Core/src/main/java/ir/vasl/chatkitv4core/model/chatkitv4enums/MessageContentType.kt
@@ -1,6 +1,7 @@
 package ir.vasl.chatkitv4core.model.chatkitv4enums
 
 enum class MessageContentType(key: String, value: Int) {
+    UNDEFINED("UNDEFINED", -1),
     TEXT("TEXT", 0),
     IMAGE("IMAGE", 1),
     VIDEO("SYSTEM", 2),
